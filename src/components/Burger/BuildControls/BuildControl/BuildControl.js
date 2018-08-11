7e02126bb27ac0b672a08677b7168df3fdb68174
gutiantian123Abc <william.guxiangtian@gmail.com>
2018-08-10 23:27:15 -0400

    Add and remove button finished

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,8 +4,8 @@ import classes from './BuildControl.css';
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
-        <button className={classes.More}>Less</button>
-        <button className={classes.Less}>More</button>
+        <button className={classes.More} onClick={props.added}>More</button>
+        <button className={classes.Less} onClick={props.removed}>Less</button>
     </div>
 );
 
